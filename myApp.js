0c7ac88092e96e99449d1e6ec784eb16bfe4ee87
lngrss <magdalenagrossmann@gmx.de>
2020-06-28 21:06:12 +0200

    Update route json

--- myApp.js
+++ myApp.js
@@ -26,7 +26,7 @@ app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 app.get('/json', function(req, res) {
-    res.json('{"message": "Hello json"}');
+    res.json({"message": "Hello json"});
 })
 
 /** 6) Use the .env file to configure the app */
