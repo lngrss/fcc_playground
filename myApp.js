bc85e0e24fce688e70be94604a43b19cbf390fa6
lngrss <magdalenagrossmann@gmx.de>
2020-06-29 21:14:52 +0200

    Add name route

--- myApp.js
+++ myApp.js
@@ -60,7 +60,9 @@ app.get('/:word/echo', function(req, res) {
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get(function(req, res) {
+    res.json({"name": req.query.first_name + ' ' + req.query.last_name});
+})
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
